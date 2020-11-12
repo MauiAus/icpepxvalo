dd62677349175c5f29c7aa452966f47e1ae3e9de
Mark <markaustin017@gmail.com>
2020-11-13 01:39:11 +0800

    Added schedule

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,16 +1,16 @@
 <template>
     <div id="schedBox">
-        <div id="navBox">
-            <p>Group A</p>
-            <p>Group B</p>
-            <p v-on:click="loadData()">Finals</p>
-        </div>
     </div>
 </template>
 
 <script>
 export default {
     name:"Schedule",
+    data(){
+        return{
+            schedList:[],
+        }
+    },
     methods:{
         loadData(){
             const GoogleSpreadsheet = require('google-spreadsheet');
@@ -18,29 +18,37 @@ export default {
 
             const creds = require('./client_secret.json');
 
+            var tempData=[];
+
             async function accessSpreadsheet(){
-                const doc = new GoogleSpreadsheet('1qypN_UXvLiJYZifBjBl7zx1_TUw9XoEXr1J4ejVqBeE');
+                const doc = new GoogleSpreadsheet('1c1HXy7YA9ycbMr4N5z-rqlp_ZAwWARYWL0ckKp1LyTw');
                 await promisify(doc.useServiceAccountAuth)(creds);
                 const info = await promisify(doc.getInfo)();
                 const sheet = info.worksheets[0];
-                console.log(sheet.title); 
-            }
+                
+                const rows = await promisify(sheet.getRows)({
+                    offset: 1
+                });
 
+                rows.forEach( row => {
+                    tempData.push({day:row.day,date:row.date,time:row.time,teama:row.teama,ascore:row.ascore,bscore:row.bscore,teamb:row.teamb,type:row.type});
+                })
+            }
             accessSpreadsheet();
-        }
+            this.schedList = tempData;
+            console.log(this.schedList)
+        },
+    },
+    beforeMount(){
+        this.loadData();
     }
 }
 </script>
 
 <style lang="scss" scoped>
 #schedBox{
-    #navBox{
-        display:flex;
-        background-color: chartreuse;
-        p{
-            padding:0% 2%;
-        }
-    }
+    float:left;
+    width:100%;
 }
 </style>
 
